17a0127b9544cbf88accbfbbddf7063f980d2029
Aldlanga80 <aldo.d.langa@gmail.com>
2025-10-07 10:58:35 -0300

    anadido de updateproduct y deleteProduct

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -14,3 +14,10 @@ export const getProductsById = async (id: string) => {
   return await Product.findById(id);
 };
 
+export const updateProduct = async (id: string, data: Partial<IProduct>) => {
+  return await Product.findByIdAndUpdate(id, data, { new: true });
+};
+
+export const deleteProduct = async (id: string) => {
+  return await Product.findByIdAndDelete(id);
+};
