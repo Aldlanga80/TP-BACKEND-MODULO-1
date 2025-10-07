359512f023e59e8935d349043147fd1f94358603
Aldlanga80 <aldo.d.langa@gmail.com>
2025-10-07 10:52:43 -0300

    Añadido de getProduts y getProductById

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -4,4 +4,13 @@ import type { IProduct } from "../models/product";
 export const createProduct = async () => {
   const product = new Product();
   return await product.save();
-}
+};
+
+export const getProducts = async () => {
+  return await Product.find();
+};
+
+export const getProductsById = async (id: string) => {
+  return await Product.findById(id);
+};
+
